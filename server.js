424b5443b0bebcf1f2809aefd43e3a185c720e0d
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2022-11-30 14:09:48 -0500

    defined dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+import roll from "./lib/roll.js"
+import minimist from "minimist"
+import express from "express"
+
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port ? args.port : 5555;
+
